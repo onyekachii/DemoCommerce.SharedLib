4ada61a369c674380f05af42b5a551c8fdc5fcac
Kachii007 <ugwu_kennedy@yahoo.com>
2025-04-08 15:52:43 +0100

    Add logger functionality

--- eCommerce.SharedLibrary/DependencyInjection/SharedServiceContainer.cs
+++ eCommerce.SharedLibrary/DependencyInjection/SharedServiceContainer.cs
@@ -6,7 +6,6 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Serilog;
-using System.Runtime.CompilerServices;
 
 namespace eCommerce.SharedLibrary.DependencyInjection
 {
---
+++ eCommerce.SharedLibrary/LogUtil/LogException.cs
@@ -0,0 +1,18 @@
+﻿using Serilog;
+
+namespace eCommerce.SharedLibrary.Logs
+{
+    public static class LogException
+    {
+        public static void LogExceptions(Exception e)
+        {
+            LogToFile(e.ToString());
+            LogToDebugger(e.ToString());
+            LogToConsole(e.ToString());
+        }
+        public static void LogToFile(string msg) => Log.Information(msg);
+        public static void LogToDebugger(string msg) => Log.Debug(msg);
+        public static void LogToConsole(string msg) => Log.Warning(msg);
+
+    }
+}
