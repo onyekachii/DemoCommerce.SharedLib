d0f882c026b45b32d3ca9e2d57be6aac52f5e3f4
Kachii007 <ugwu_kennedy@yahoo.com>
2025-04-11 00:19:19 +0100

    Additional Dtos

---
+++ eCommerce.SharedLibrary/DTO/AppUserDto.cs
@@ -0,0 +1,16 @@
+﻿
+
+using System.ComponentModel.DataAnnotations;
+
+namespace eCommerce.SharedLibrary.DTO
+{
+    public record AppUserDto(
+        int Id,
+        [Required]string Name,
+        [Required] string Phone,
+        [Required, EmailAddress] string Email,
+        [Required] string Password,
+        [Required] string Address,
+        [Required] string Role
+        );
+}
---
+++ eCommerce.SharedLibrary/DTO/OrderDetailsDto.cs
@@ -0,0 +1,18 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace eCommerce.SharedLibrary.DTO
+{
+    public record OrderDetailsDto(
+        [Required]int OrderId,
+        [Required] int ProductId,
+        [Required] int ClientId,
+        [Required] int PurchasedQuantity,
+        [Required] DateTime OrderedDate,
+        [Required] string ProductName,
+        [Required] string Phone,
+        [Required, EmailAddress] string Email,
+        [Required, DataType(DataType.Currency)] decimal UnitPrice,
+        [Required, DataType(DataType.Currency)] decimal TotalPrice
+    );
+}
